b5410d421c6936cd19179ecc4d44f8a9da679934
Palash Roy <cstpalash@gmail.com>
2018-12-17 09:52:00 +0530

    homepage fix

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 
 const styles = theme => ({
-  infoGrid: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+  root: {
+    flexGrow: 1
   },
-  mainFeaturedPost: {
+  infoGrid: {
     backgroundColor: theme.palette.grey[800],
     color: theme.palette.common.white,
-    marginBottom: theme.spacing.unit * 4,
-    height: 400
+    textAlign: 'center',
   },
+  middleGrid: {
+    paddingTop: 100
+  },
+  codeGrid: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+  }
 });
 
 export class Home extends React.Component{
@@ -25,19 +30,23 @@ export class Home extends React.Component{
       const { classes } = this.props;
 
       return (
-        <Grid container spacing={24} className={classes.mainFeaturedPost}>
-          <Grid item xs={12} md={6} className={classes.infoGrid}>
-            
-            <Typography variant="h4" color="inherit" gutterBottom>
-            {'<>'} CodeComp on Fabric
-            </Typography>
-
-          </Grid>
-          <Grid item xs={12} md={6}>
+        <div className={classes.root}>
+          <Grid container spacing={24}>
+            <Grid item xs={12} md={6} className={classes.infoGrid}>
+              <div className={classes.middleGrid}>
+                <Typography variant="h3" color="inherit" gutterBottom>
+                  {'<>'} CodeComp on Fabric
+                </Typography>
+                <Typography variant="h6" color="inherit" gutterBottom>
+                  Participate in an algorithmic battle
+                </Typography>
+                <Button variant="contained" color="secondary" href="/register">Register</Button>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.codeGrid}>
               <pre>{`
 public static intelligent main(string[] args) \{
   console.WriteLine(env.FIRST_EVER_ALGO_BATTLE_ON_FABRIC); //true
-  
   me.Wow();
   me.Register(); //individual or team
   if(!me.DO_I_KNOW_FABRIC) me.QuickLearn(env.FABRIC); //1 day
@@ -58,10 +67,10 @@ public static intelligent main(string[] args) \{
   else me.WaitForNext();
 }
 
-              `}</pre>
-            
-          </Grid>      
-        </Grid>
+`             }</pre>
+            </Grid>
+          </Grid>
+        </div>
       );
     }
   
